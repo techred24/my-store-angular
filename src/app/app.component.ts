81b30072399be5b71834d1abdba9f466f7f2e65a
Rafael <f.rafael.temich24@gmail.com>
2023-01-02 14:50:17 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  public name: string = 'Flubber';
+  public age: number = 18;
+  public img: string = 'https://source.unsplash.com/random';
+  public btnDisabled: boolean = false;
+  public person: Persona = {
+    name: 'El flubber',
+    age: 19,
+    avatar: 'https://source.unsplash.com/random'
+  }
+  public toggleButton(): void {
+    this.btnDisabled = !this.btnDisabled;
+  }
+  public increaseAge(): void {
+    if (this.person.age !== undefined) this.person.age += 1;
+  }
+  public onScroll(event: Event): void {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+  public changeName(event: Event): void {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+}
+interface Persona {
+  name?: string;
+  age?: number;
+  avatar?: string;
 }
